c7b64f46470ce81f326286f0d2b56250ded4df02
mjt0704 <mjt0704@gmail.com>
2014-03-19 16:52:27 +0800

    Initial Commit

---
+++ OSXSlightlyBetterAuth/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  OSXSlightlyBetterAuth
+//
+//  Created by Maojie Tang on 14-3-19.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
